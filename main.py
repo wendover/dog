1e0746ba694c6403cfa93993ce255ae7e24119ec
naoki okamoto <wendover42@gmail.com>
2022-01-25 11:23:23 +0900

    add item page

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def hello():
+@app.route("/", methods=["GET"])
+def main():
     return render_template("dog.html")
+
+@app.route("/item", methods=["GET"])
+def item():
+    return render_template("item.html")
 
 ## おまじない
 if __name__ == "__main__":
